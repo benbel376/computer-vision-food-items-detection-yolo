cd1510360716221650cdaedfabf26f0a44ca9f7a
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2024-10-11 16:00:20 +0300

    create the pipeline scripts

---
+++ src/app/pipeline/basic.py
@@ -0,0 +1,96 @@
+import sys
+import logging
+import os
+from datetime import datetime
+from from_root import from_root
+import os.path
+import sys
+import yaml
+import base64
+
+from wasteDetection.exception import AppException
+from wasteDetection.logger import logging
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            logging.info("Read yaml file successfully")
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise AppException(e, sys) from e
+    
+
+
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+            logging.info("Successfully write_yaml_file")
+
+    except Exception as e:
+        raise AppException(e, sys)
+    
+
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open("./data/" + fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
+
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+
+log_path = os.path.join(from_root(), 'log', LOG_FILE)
+
+os.makedirs(log_path, exist_ok=True)
+
+lOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=lOG_FILE_PATH,
+    format= "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    level= logging.INFO
+)
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+
+    return error_message
+
+
+class AppException(Exception):
+    def __init__(self, error_message, error_detail):
+        """
+        :param error_message: error message in string format
+        """
+        super().__init__(error_message)
+
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail
+        )
+
+    def __str__(self):
+        return self.error_message
